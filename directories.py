5b905d60b36ef3b9f866d916cd510511cb84ddb6
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2024-11-21 14:54:25 +0100

    Add files via upload

---
+++ directories.py
@@ -0,0 +1,21 @@
+import os
+
+
+def dir(path):
+    # This will print out all directories in path.
+    direc = os.listdir(path)
+    # Going through all files in path.
+    for i in direc:
+        # Adding the previous directories in front of path
+        # so that it can be called upon globally.
+        path_dir = os.path.join(path, i)
+        # If we stumble upon a file we call upon the function again
+        # so that we can repeat the procedure for all nested dir.
+        if os.path.isdir(path_dir):
+            dir(path_dir)
+        # We just print if it's a file.
+        elif os.path.isfile(path_dir):
+            print(i) 
+
+
+dir('/home/kalle/Dokument')
