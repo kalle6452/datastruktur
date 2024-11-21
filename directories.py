9788d79455ad5893a08c970ebb59a4c9e352bccf
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2024-11-21 14:59:42 +0100

    Add files via upload

--- directories.py
+++ directories.py
@@ -6,16 +6,18 @@ def dir(path):
     direc = os.listdir(path)
     # Going through all files in path.
     for i in direc:
-        # Adding the previous directories in front of path
-        # so that it can be called upon globally.
-        path_dir = os.path.join(path, i)
-        # If we stumble upon a file we call upon the function again
-        # so that we can repeat the procedure for all nested dir.
-        if os.path.isdir(path_dir):
-            dir(path_dir)
-        # We just print if it's a file.
-        elif os.path.isfile(path_dir):
-            print(i) 
+        # if filename starts with . it is hidden file.
+        if not i.startswith('.'):
+            # Adding the previous directories in front of path
+            # so that it can be called upon globally.
+            path_dir = os.path.join(path, i)
+            # If we stumble upon a file we call upon the function again
+            # so that we can repeat the procedure for all nested dir.
+            if os.path.isdir(path_dir):
+                dir(path_dir)
+            # We just print if it's a file.
+            elif os.path.isfile(path_dir):
+                print(i) 
 
 
 dir('/home/kalle/Dokument')
