66f2975926158d920b590fbf57c25e25cee26a20
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2024-11-22 20:58:31 +0100

    Update brute_force_test.py

--- brute_force_test.py
+++ brute_force_test.py
@@ -1,3 +1,12 @@
+import random
+import time
+start_time = time.time()
+# lst = [0, 0, 0]
+lst = [-1,0,1,2,-1,-4]
+# lst = [0,0,0]
+# lst = random.sample(range(1, 10**15), 1000)
+
+
 def threesum_brute(lst):
     # There has to be at least 3 elements in original list.
     if len(lst) < 3:
@@ -7,15 +16,19 @@ def threesum_brute(lst):
     n = len(lst)
     # We compute all possible combinations with 3 for-loops.
     # We use slicing so that redundant operations aren't executed.
-    for i in lst[:n-2]:
-        print(i)
-        for j in lst[i+1:n-1]:
-            for k in lst[j+1:n]:
-                if i+j+k == 0 and i != j and i != k:
-                    # We make it a tuple so that it can become a set.
+    for idi, i in enumerate(lst):
+        for idj, j in enumerate(lst):
+            for idk, k in enumerate(lst):
+                # if i+j+k == 0 and lst.index(i) != lst.index(j) and lst.index(i) != lst.index(k):
+                if i+j+k == 0 and idi != idj and idi != idk and idj != idk:
                     lista.append(tuple(sorted([i, j, k])))
+                    # We make it a tuple so that it can become a set.
+                    # lista.append(tuple(sorted([lst.index(i), lst.index(j), lst.index(k)])))
+                    #lista.append(tuple(sorted([lst[lst.index(i)], lst[lst.index(j)], lst[lst.index(k)]])))
     # Removing duplicates
     return list(set(lista))
 
-    
-print(threesum_brute([-1, 0, 1, 2, -1, 4]))
+
+print(len(lst))    
+print(threesum_brute(lst))
+print("--- %s seconds ---" % (time.time() - start_time))
