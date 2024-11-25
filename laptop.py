0196f9d8a93eb64ac23dcdca5fa58c4ee2faeb3b
kalle6452 <70005565+kalle6452@users.noreply.github.com>
2024-11-25 21:52:23 +0100

    Create laptop.py

---
+++ laptop.py
@@ -0,0 +1,46 @@
+import time
+import random
+from sort_algorithms import (selection, bubble, insertion_2, 
+                             insertion_3, insertion)
+from matplotlib import pyplot as plt
+colors = ['green', 'blue', 'red']
+for y in range(100, 3500, 100):
+    for i in range(3):
+        if i == 0:
+            lst = random.sample(range(-10**5, 10**5), y)
+            start_time = time.time()
+            selection(lst)
+            if y == 100:
+                (plt.scatter(y, (time.time() - start_time), 
+                             c='green', label='selection'))
+            else:
+                (plt.scatter(y, (time.time() - start_time), 
+                             c='green'))
+        if i == 1:
+            lst = random.sample(range(-10**5, 10**5), y)
+            start_time = time.time()
+            bubble(lst)
+            if y == 100:
+                (plt.scatter(y, (time.time() - start_time), 
+                             c='red', label='bubble'))
+            else:
+                (plt.scatter(y, (time.time() - start_time), 
+                             c='red'))
+        if i == 2:
+            lst = random.sample(range(-10**5, 10**5), y)
+            start_time = time.time()
+            insertion(lst)
+            if y == 100:
+                (plt.scatter(y, (time.time() - start_time), 
+                             c='blue', label='insertion'))
+            else:
+                (plt.scatter(y, (time.time() - start_time), 
+                             c='blue'))
+
+
+plt.yscale('log')
+plt.xscale('log')
+plt.xlabel('List size')
+plt.ylabel('Run time')
+plt.legend()
+plt.show()
